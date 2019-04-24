7a69a89f98d9e039cda9edf5fdf6f2afaaef5a8e
Jonathan Bennett <jonathanb@indeed.com>
2019-04-24 08:06:47 -0500

    Add main()

--- game.py
+++ game.py
@@ -1,33 +1,39 @@
 import pygame
+import os
 from board import Board
 
 pygame.init()
-game_display = pygame.display.set_mode((1200, 1200))
-pygame.display.set_caption("Chu Shogi")
+
+board = pygame.transform.scale(pygame.image.load(os.path.join("img","board.png")), (1000, 1000))
+
+
+
 clock = pygame.time.Clock()
 
 chu_shogi_board = Board()
 chu_shogi_board.create_board()
 chu_shogi_board.print_board()
 
-all_tiles = []
-all_pieces = []
-
-pygame.draw.rect(game_display, (100, 100, 100), [0, 0, 100, 100])
-
-def squares(x, y, w, h, color):
-    pygame.draw.rect(game_display, color, [x, y, w, h])
-    all_tiles.append([color, [x, y, w, h]])
 
 
-quit_game = False
+def main():
+    quit_game = False
 
-while not quit_game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit_game = True
-            quit()
-            pygame.quit()
+    while not quit_game:
 
-    pygame.display.update()
-    clock.tick(60)
+        win.blit(board, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game = True
+                quit()
+                pygame.quit()
+
+            pygame.display.update()
+            clock.tick(60)
+
+width = 1000
+height = 1000
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Chu Shogi")
+main()
